4f419872950187ca9ff50047d8a0ed23b2161f11
Andrew Zajac <ajzajac@icloud.com>
2019-08-12 18:40:23 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,3 +10,17 @@ def map(array)
   newarray
 end
 
+def reduce(array, value = nil)
+  if value
+    reduced = value
+    i = 0
+  else
+    reduced = array[0]
+    i = 1
+  end
+  while i < array.length
+    reduced = yield(reduced, array[i])
+    i += 1
+  end
+  return reduced
+end
