ebd3c3e6e50ceed54f9176b93bad64aeba7db603
Andrew Zajac <ajzajac@icloud.com>
2019-08-12 18:39:01 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,11 +1,11 @@
 # Your Code Here
 
 def map(array)
-  new_array = []
-  counter = 0
+  newarray = []
+  i = 0
   while counter < array.length do
-    new_array.push(yield(array[counter]))
-    counter += 1
+    newarray.push(yield(array[i]))
+    i += 1
   end
-  new_array
+  newarray
 end
