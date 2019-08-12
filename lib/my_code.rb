a56aedc6018cf9f4baf6d98b6d860f1cdc951429
Andrew Zajac <ajzajac@icloud.com>
2019-08-12 18:48:09 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,35 +10,20 @@ def map(array)
   newarray
 end
 
-def reduce(array, starting_value=0)
-  if starting_value
-    reduced = starting_value
-    i = 0
-  else
-    reduced = array[0]
-    i = 1
-  end
-  while i < array.length
-    reduced = yield(reduced, array[i])
-    i += 1
-  end
-  return reduced
-end
-
 def reduce(array, starting_point=0)
   if starting_point == 0 
     starting_point = array[0]
     i = 1
-    while i<array.length do
+    while i < array.length do
       starting_point = yield(starting_point, array[i])
-      i+=1
+      i += 1
     end
     return starting_point
   else
     i = 0
-    while i<array.length do
+    while i < array.length do
       starting_point = yield(starting_point, array[i])
-      i+=1
+      i += 1
     end
     return starting_point
   end
