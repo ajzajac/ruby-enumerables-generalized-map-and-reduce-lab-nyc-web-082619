8b9fbb43ec6cebbd97f50ba55ad0c9a41c2df26a
Andrew Zajac <ajzajac@icloud.com>
2019-08-12 18:47:11 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -25,3 +25,20 @@ def reduce(array, starting_value=0)
   return reduced
 end
 
+def reduce(array, starting_point=0)
+  if starting_point == 0 
+    starting_point = array[0]
+    i = 1
+    while i<array.length do
+      starting_point = yield(starting_point, array[i])
+      i+=1
+    end
+    return starting_point
+  else
+    i = 0
+    while i<array.length do
+      starting_point = yield(starting_point, array[i])
+      i+=1
+    end
+    return starting_point
+  end
